981ba0a6d35ffc387e8becf8ec6313712b1a1a21
angelfish2510 <angelisa_herring@yahoo.com>
2022-02-17 06:26:59 -0800

    comment cleanup

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let liveHeart = scoreboardLives.firstChild;
 // let liveHeart = document.querySelector('.tries')
 
 
-//declare and initialize the phrases array, storing at least five strings that contain only letters and spaces, no punctuation
+
 const myPhrases = ['Bunnies are the best',
                  'Belize is incredible',
                  'Apple starts with A',
@@ -28,56 +28,34 @@ const myPhrases = ['Bunnies are the best',
                  'Always take the time to stop and smell the roses'
 ];
 
-//Add the event listener to the variable startButton
-//Hide the overlay by changing its display property
 
-startButton.addEventListener('click', (e) => {
-    overlay.style.display = 'none';
-});
 
-// Create a getRandomPhraseAsArray function
 function getRandomPhrasesAsArray(arr) {
-    // Create a variable to store a random number based on the length of the array
     const randomNumber = Math.floor( Math.random() * arr.length );
     const randomPhrase = arr[randomNumber].toLowerCase();
-    // Use the variable to select an index inside of the array.
     const charactersInPhrase = randomPhrase.split('');
     console.log(charactersInPhrase);
     return charactersInPhrase;
 }
 
-// After you create the getRandomPhraseArray, you will need to "call" it, and pass the phrases array to it.
 let charactersInRandomPhrase = getRandomPhrasesAsArray(myPhrases);
-let charactersInRandomPhrase2 = getRandomPhrasesAsArray(myPhrases);
 
 
-// Create an addPhraseToDisplay function that loops through an array of characters. You will need to write it so that it can take any array of letters and add it to the display.
+
 function addPhraseToDisplay(arr) {
-//  Inside the loop, for each character in the array,
     for (let i = 0; i < arr.length; i++) {
-// ❏ Create a list li item
-  const li = document.createElement('li');
-// ❏ Put the character inside of the list item
-    console.log( li.textContent = arr[i] );
-// ❏ Append that list item to the #phrase ul in your HTML
-    phrase.appendChild(li);
-// ❏ If the character in the array is a letter and not a space, the
-// function should add the class “letter” to the list item. If not, add
-// the “space” class.
-if (arr[i] !== ' ') {
-      li.classList.add('letter');
-//   } else if (arr[i] === ' ') {
-      } else {
-      li.classList.add('space');
-  }
- }
+        const li = document.createElement('li');
+        console.log( li.textContent = arr[i] );
+        phrase.appendChild(li);
+            if (arr[i] !== ' ') {
+        li.classList.add('letter');
+            } else {
+            li.classList.add('space');
+        }
+    }
 }
 
-// To use the function, you’ll get the value returned by the getRandomPhraseAsArray, save it to a variable, and pass it to addPhraseToDisplay as an argument.
-// let charactersInRandomPhrase = getRandomPhrasesAsArray(myPhrases);
 addPhraseToDisplay(charactersInRandomPhrase);
-
-
 
 
 // Create a function “stub” for the checkLetter function
@@ -157,60 +135,7 @@ const checkWin = () => {
 
 
 
-// Start by creating an event listener for the qwerty element that listens for the
-// “click” event.
-qwerty.addEventListener('click', (e) => {
-    let button = e.target;
-    let livesRemaining;
-    console.log(button.innerText);
 
-
-    // ❏ Use a conditional to filter out clicks that don’t happen on the buttons or if the
-// button already has the “chosen” class
-    if ( button.tagName !== 'BUTTON' || button.className === "chosen" ) {
-        button.disabled = true;
-        alert("Please select a letter.");
-    } else {
-        // button.disabled = false;
-        // ❏ Add the “chosen” class to the button that was pressed.
-        button.classList.add("chosen");
-// ❏ Call the checkLetter function and store the results in a variable.
-        let letterFound = checkLetter(button);
-// ❏ If the checkLetter function does not find a letter, remove one of the heart
-// images and increment the missed counter
-
-        if (letterFound === null) {    
-            missed++;
-            document.querySelector('.tries img').src = "images/lostHeart.png";
-            document.querySelector('.tries').className = 'fail';
-            checkWin();
-            if (missed > 1 && missed < 5) {
-                livesRemaining = 5 - missed;
-                alert(`Darn... you lost a life, but I believe in you!  You still have ${livesRemaining} more tries... you can do it!`);
-
-            } else if  (missed === 4 ) {
-                livesRemaining = 5 - missed;
-                alert(`Darn... you lost a life, but I believe in you!  You still have ${livesRemaining} more try... you can do it!`);
-               }
-
-
-        // } else if (!win) {
-
-        } else {
-            checkWin();
-            if (won === 'yes') {
-                return;
-            }
-            let livesRemaining;
-            // missed = missed;
-            livesRemaining = 5 - missed;
-            
-            alert(`Way to go!  Select another letter to continue playing.  You still have ${livesRemaining} more tries... you can do it!`);
-        // }   else {
-            // tryAgain();
-        }
-      }
-});
 
 
 function playAgain () {
@@ -242,3 +167,47 @@ function playAgain () {
 //     overlay.style.display = 'none';
 //     addPhraseToDisplay(charactersInRandomPhrase2);
 // });
+
+// initialize game
+
+startButton.addEventListener('click', (e) => {
+    overlay.style.display = 'none';
+});
+
+qwerty.addEventListener('click', (e) => {
+    let button = e.target;
+    let livesRemaining;
+    console.log(button.innerText);
+    if ( button.tagName !== 'BUTTON' || button.className === "chosen" ) {
+        button.disabled = true;
+        alert("Please select a letter.");
+    } else {
+        // button.disabled = false;
+        button.classList.add("chosen");
+        let letterFound = checkLetter(button);
+        if (letterFound === null) {    
+            missed++;
+            document.querySelector('.tries img').src = "images/lostHeart.png";
+            document.querySelector('.tries').className = 'fail';
+            checkWin();
+            if (missed > 1 && missed < 5) {
+                livesRemaining = 5 - missed;
+                alert(`Darn... you lost a life, but I believe in you!  You still have ${livesRemaining} more tries... you can do it!`);
+
+            } else if  (missed === 4 ) {
+                livesRemaining = 5 - missed;
+                alert(`Darn... you lost a life, but I believe in you!  You still have ${livesRemaining} more try... you can do it!`);
+               }
+            } else {
+            checkWin();
+            if (won === 'yes') {
+                return;
+            }
+            let livesRemaining;
+            // missed = missed;
+            livesRemaining = 5 - missed;
+            
+            alert(`Way to go!  Select another letter to continue playing.  You still have ${livesRemaining} more tries... you can do it!`);
+        }
+      }
+});
